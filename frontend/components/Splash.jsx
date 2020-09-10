5a0168d6e05441b5ec69c23eacd71aacc74da569
joe bumbaca <joebumbaca@gmail.com>
2020-09-10 13:40:57 -0700

    Finish Splash Page additions

--- frontend/components/Splash.jsx
+++ frontend/components/Splash.jsx
@@ -2,29 +2,54 @@ import React from 'react';
 import GreetingContainer from './greeting/greeting_container';
 
 const Splash = () => (
-<div>
-  <header>
-    <GreetingContainer />
-  </header>
-
-  <div className="main">
-    <div className='main-top'>
-      <div className="main-left">
-        <div className="yellow-div">
-        </div>
-        <div className="splash-text">
+  <>
+    <section className='main-top'>
+      <GreetingContainer />
+      <section className='main-mid'>
+        <div className='main-left'>
+          <div className='yellow-div'></div>
+          <div className='splash-text'>
             Share your love of Beer with millions of your closest friends.
+          </div>
         </div>
+        <div className='main-right'></div>
+      </section>
+    </section>
+    <section className='main-bottom'>
+      <div className='bottom-images'>
+        <img src={window.images.ratings} alt='ratings' />
+        <img src={window.images.badges} alt='badges' />
       </div>
-      <div className="main-right">
-      </div>
-    </div>
-    <div className='main-bottom'>
-      
-    </div>
-  </div>
-</div>
-
+      <section className='bottom-images'>
+        <p className='bottom-images-header'>CHECK IN AND RATE BEER</p>
+        <p className='bottom-images-header'>DRINK NEW BEERS, UNLOCK BADGES</p>
+      </section>
+      <section className='bottom-images'>
+        <p className='bottom-images-text'>
+          Keep track of what you’ve tried and what you thought of it by checking
+          in a beer and rating it.
+        </p>
+        <p className='bottom-images-text'>
+          Expand your palate by trying new & different beer styles and unlock
+          achievements along the way.
+        </p>
+      </section>
+      <section className='footer'>
+        <a href='https://github.com/JoeBumbaca'>
+          <i className='fab fa-github fa-4x'></i>
+        </a>
+        <a href='https://www.linkedin.com/in/joseph-bumbaca-b4591568/'>
+          <i className='fab fa-linkedin fa-4x'></i>
+        </a>
+        <a href='https://angel.co/u/joe-bumbaca'>
+          <i className='fab fa-angellist fa-4x'></i>
+        </a>
+        <a href='https://joebumbaca.com/'>
+          <i className='fas fa-gem fa-4x'></i>
+        </a>
+      </section>
+    </section>
+  </>
 );
 
 export default Splash;
--- frontend/components/nav/nav.jsx
+++ frontend/components/nav/nav.jsx
@@ -11,21 +11,6 @@ class NavBar extends React.Component {
     }
 
     this.handleLogout = this.handleLogout.bind(this);
-    this.handleDropDownEnter = this.handleDropDownEnter.bind(this);
-    this.handleDropDownLeave = this.handleDropDownLeave.bind(this);
-  }
-
-
-  handleDropDownEnter() {
-    this.setState({
-      dropDown: true
-    })
-  }
-
-  handleDropDownLeave() {
-    this.setState({
-      dropDown: false
-    })
   }
 
   handleLogout(e) { 
@@ -35,37 +20,49 @@ class NavBar extends React.Component {
   }
 
   render () {
-    const dropDownMenu = this.state.dropDown ? <div className='nav-dropdown' onMouseLeave={this.handleDropDownLeave}>Hello</div> : null
+    
     if (this.props.currentUser) {
     return (
-      <div className="nav-bar">
-        <div className="left-inner-nav"></div>
-        <div className="inner-nav-bar">
-        <div className="logo-title">
-        <img className="nav-logo" src={window.navLogoURL}/>
-        <div className="nav-title">
-              <Link to="/the_pub" className="navLink">UNCAPPD</Link>
+      <div className='nav-bar'>
+        <div className='left-inner-nav'></div>
+        <div className='inner-nav-bar'>
+          <div className='logo-title'>
+            <img className='nav-logo' src={window.navLogoURL} />
+            <div className='nav-title'>
+              <Link to='/the_pub' className='navLink'>
+                UNCAPPD
+              </Link>
+            </div>
+          </div>
+          <ul className='nav-links'>
+            <li className='nav-beers'>
+              <Link to='/breweries' className='navLink'>
+                Breweries
+              </Link>
+            </li>
+            <li className='nav-reviews'>
+              <Link to='/the_pub' className='navLink'>
+                The Pub
+              </Link>
+            </li>
+            <li className='nav-users'>
+              <Link to='/beers' className='navLink'>
+                Beers
+              </Link>
+            </li>
+          </ul>
+          <div className='user-button'>
+            <div className='current-user'>
+              <i className='fas fa-user-circle fa-2x'></i>
+              {this.props.currentUser.username}
+            </div>
+            <button className='nav-logout' onClick={this.handleLogout}>
+              Logout
+            </button>
+          </div>
         </div>
-        </div>
-        <ul className="nav-links">
-          <li className="nav-beers">
-              <Link to="/breweries" className="navLink">Breweries</Link>
-          </li>
-          <li className="nav-reviews">
-            <Link to="/the_pub" className="navLink">The Pub</Link>
-          </li>
-          <li className="nav-users">
-              <Link to="/beers" className="navLink">Beers</Link>
-          </li>
-        </ul>
-        <div className="user-button">
-          <div className="current-user" onMouseEnter={this.handleDropDownEnter} onMouseLeave={this.handleDropDownLeave}>{this.props.currentUser.username} </div>
-          <button className="nav-logout" onClick={this.handleLogout}>Logout</button>
-        </div>
-        </div>
-        {dropDownMenu}
       </div>
-    )
+    );
     } else {
       return null;
     };
